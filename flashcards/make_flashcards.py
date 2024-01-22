76d16bdd99d89784b76fe545b6cc4035ef215071
Isaac Caswell <isaaccaswell91.com>
2024-01-21 23:19:04 -0800

    meow

--- flashcards/make_flashcards.py
+++ flashcards/make_flashcards.py
@@ -7,13 +7,21 @@ import pinyin_jyutping_sentence
 fname_out="flashcards/sample1.flashcards.csv"
 
 
+# TSV: first col is the target ci
+input_ci_fname = "resources/hsk1to6_zi_pinyin_def_sample.tsv"
+
+# TSV: maps ci to (short) definition
+definitions_fname = "resources/hsk1to6_zi_pinyin_def.tsv"
+
 pinyin_fname = "resources/hsk1to6_zi_pinyin_def.tsv"
 zi_definitions_fname = "resources/zi_singleword_defs.tsv"
 use_same_zi_fname = "resources/use_same_zi/mega_hanzi.tsv"
 
-relatedwords_fname = "resources/related_words/hsk1to6.tsv"
-relatedwords_explanation_fname = "output/hsk1to6.tsv__gpt-3.5-turbo-1106_t0.0__related_words_prompt__full.txt"
-examples_fname ="resources/example_sentences/cql.tsv"
+# relatedwords_fname = "resources/related_words/hsk1to6.tsv"
+relatedwords_explanation_fname = "resources/hsk1to6.tsv__gpt-3.5-turbo-1106_t0.0__related_words_prompt.csv"
+examples_fnames = [
+        ["🐇", 2, "resources/example_sentences/cql.tsv"]
+]
 NEWLINE = "\r"
 
 def somple(X):
@@ -21,19 +29,32 @@ def somple(X):
   print("\n=================================")
   print(x[0:5], x[-5:])
 
+TARGET_CI = []
+with open(input_ci_fname, "r") as f:
+  for line in f:
+    TARGET_CI.append(line.strip().split("\t")[0])
+
+DEFINITIONS = {}
+with open(definitions_fname, "r") as f:
+  for line in f:
+    parts = line.strip().split("\t")
+    if len(parts) >=3:
+      DEFINITIONS[parts[0]] = parts[2]
+print("DEFINITIONS" + "\n"); somple(DEFINITIONS)
+
 PINYIN = {}
 with open(pinyin_fname, "r") as f:
   for line in f:
     parts = line.strip().split("\t")
     PINYIN[parts[0]] = parts[1]
-somple(PINYIN)
+print("PINYIN" + "\n"); somple(PINYIN)
 
 ZI_DEFS = {}
 with open(zi_definitions_fname, "r") as f:
   for line in f:
     parts = line.strip().split("\t")
     ZI_DEFS[parts[0]] = parts[1]
-somple(ZI_DEFS)
+print("ZI_DEFS" + "\n"); somple(ZI_DEFS)
 
 # i zi  first_occurence hsk1  hsk2  hsk3  hsk4  hsk5  hsk6  nhsk1 nhsk2 nhsk3 nhsk4 nhsk5 nhsk6 stront1 weeb1  
 USE_SAME_ZI = {}
@@ -41,63 +62,47 @@ with open(use_same_zi_fname, "r") as f:
   for line in f:
     parts = line.strip().split("\t")
     USE_SAME_ZI[parts[1]] = parts[-1]  # TODO handle logic for only the level below and equal
-somple(USE_SAME_ZI)
+print("USE_SAME_ZI" + "\n"); somple(USE_SAME_ZI)
+
+# RELATED_WORDS = {}
+# with open(relatedwords_fname, "r") as f:
+#   for line in f:
+#     parts = line.strip().split("\t")
+#     RELATED_WORDS[parts[0]] = parts[1]
+# somple(RELATED_WORDS)
 
 RELATED_WORDS = {}
-with open(relatedwords_fname, "r") as f:
-  for line in f:
-    parts = line.strip().split("\t")
-    RELATED_WORDS[parts[0]] = parts[1]
-somple(RELATED_WORDS)
+with open(relatedwords_explanation_fname, "r") as f:
+  reader = csv.reader(f, delimiter=';')
+  for row in reader:
+    RELATED_WORDS[row[0]] = row[1]
+print("RELATED_WORDS" + "\n"); somple(RELATED_WORDS)
 
 EXAMPLES = {}
-with open(examples_fname, "r") as f:
-  for line in f:
-    if "\t" not in line:
-      print(line)
-      continue
-    ci, content = line.strip().split("\t", maxsplit=1)
-    content = content.split("\t")
-    if len(content)%2 != 0:
-        print(content)
-        content = content[0:-1]
-    EXAMPLES[ci] = {"zh": [], "en": []}
-    for i, example in enumerate(content):
-      if i%2 == 0: EXAMPLES[ci]["zh"].append(example)
-      if i%2 == 1: EXAMPLES[ci]["en"].append(example)
-somple(EXAMPLES)
+for example_emoji, n_examples, examples_fname in examples_fnames:
+  with open(examples_fname, "r") as f:
+    for line in f:
+      if "\t" not in line:
+        # print(line)
+        continue
+      ci, content = line.strip().split("\t", maxsplit=1)
+      content = content.split("\t")
+      if len(content)%2 != 0:
+          # print(content)
+          content = content[0:-1]
+      if ci not in EXAMPLES:
+        EXAMPLES[ci] = {"zh": [], "en": []}
+      for i, example in enumerate(content):
+        if i%2 == 0: EXAMPLES[ci]["zh"].append(f"{example_emoji} {example}")
+        if i%2 == 1: EXAMPLES[ci]["en"].append(example)
+print("EXAMPLES" + "\n"); somple(EXAMPLES)
 
 
-def bold_han(s):
-  # return s
-  return re.sub("- (\p{Han}+( \([^\)]*\):)?)", "\r\\1\r", s)
-  return re.sub("(\p{Han}+( \([^\)]*\):)?)", "**\\1**", s)
+# def bold_han(s):
+#   # return s
+#   return re.sub("- (\p{Han}+( \([^\)]*\):)?)", "\r\\1\r", s)
+#   return re.sub("(\p{Han}+( \([^\)]*\):)?)", "**\\1**", s)
 
-
-out_lines = []
-
-CI_FIELD = lines[0].index("Chinese")
-PINYIN_FIELD = lines[0].index("pinyin")
-DEF_FIELD = lines[0].index("short definition")
-
-
-RELATED_WORDS_FIELD = lines[0].index("related words")
-
-ZI_FIELDS = [
-(lines[0].index("zi 1"), lines[0].index("zi 1 examples")),
-(lines[0].index("zi 2"), lines[0].index("zi 2 examples")),
-(lines[0].index("zi 3"), lines[0].index("zi 3 examples")),
-(lines[0].index("zi 4"), lines[0].index("zi 4 examples")),
-]
-
-EX_FIELDS = [
-(lines[0].index("ex 1 Chinese"), lines[0].index("ex 1 English")),
-(lines[0].index("ex 2 Chinese"), lines[0].index("ex 2 English")),
-(lines[0].index("ex 3 Chinese"), lines[0].index("ex 3 English")),
-(lines[0].index("ex 4 Chinese"), lines[0].index("ex 4 English")),
-(lines[0].index("ex 5 Chinese"), lines[0].index("ex 5 English")),
-(lines[0].index("ex 6 Chinese"), lines[0].index("ex 6 English")),
-]
 
 def pinyin(s):
   p =  pinyin_jyutping_sentence.pinyin(s)
@@ -105,41 +110,61 @@ def pinyin(s):
   p = p.replace(" dū ", " dōu ")
   return p
 
-for line in lines[1:]:
-  out_lines.append([])
-  out_lines[-1].append(line[CI_FIELD]) 
-  out_lines[-1].append(line[PINYIN_FIELD]) 
-  out_lines[-1].append(line[DEF_FIELD]) 
+def conditional_append(ci, resource, out_line, field_name=None):
+  if ci in resource:
+    if field_name: out_line[0].append(field_name)
+    out_line[0].append(resource[ci])
+    return True
+  else:
+    if "missing" not in resource:
+      resource["missing"] = []
+    resource["missing"].append(ci)
+    if field_name is not None:
+      return True
+    return False
+  
 
-  if line[RELATED_WORDS_FIELD]:
-      out_lines[-1].append("related words")
-      # out_lines[-1].append("tmp")
-      out_lines[-1].append(bold_han(line[RELATED_WORDS_FIELD]))
+out_lines = []
+for ci_j in TARGET_CI:
+  out_line = [[ci_j]]
+  if not conditional_append(ci_j, PINYIN, out_line): continue
+  if not conditional_append(ci_j, DEFINITIONS, out_line): continue
+  if not conditional_append(ci_j, RELATED_WORDS, out_line, "related words"): pass
 
-  for zi_field, other_ci_field in ZI_FIELDS:
-    if line[zi_field]:
-      out_lines[-1].append(line[zi_field])
-      if line[other_ci_field]:
-        other_ci_list = []
-        for ci in line[other_ci_field].split(";"):
-          ci = ci.strip()
-          if ci in PINYIN:
-            ci = f"{ci} ({PINYIN[ci]})"
-          other_ci_list.append(ci)
+  for zi_j in ci_j:
+    if zi_j in ZI_DEFS: out_line[0].append(f"{zi_j} ({ZI_DEFS[zi_j]})")
+    else: out_line[0].append(f"{zi_j}")
 
-        out_lines[-1].append("Other words using this character: " + "; ".join(other_ci_list))
-      else:
-        out_lines[-1].append("There are no other HSK words in this level (or before) using this character.")
+    other_ci_list = []
+    other_ci_superset = USE_SAME_ZI[zi_j].split(";") if zi_j in USE_SAME_ZI else []
+    for ci_k in other_ci_superset:
+      ci_k = ci_k.strip()
+      if ci_k == ci_j: continue
+      if len(ci_k) > 4: continue
+      if ci_k in PINYIN: ci_k = f"{ci_k} ({PINYIN[ci_k]})"
+      other_ci_list.append(ci_k)
+    if other_ci_list:
+      out_line[0].append("Other words using this character: " + "; ".join(other_ci_list))
+    else:
+      out_lines[0].append("There are no other HSK words in this level (or before) using this character.")
 
-  seen_examples = set()
-  for ex_zh_field, ex_en_field in EX_FIELDS:
-    if line[ex_zh_field].startswith("#"): continue
-    if line[ex_zh_field]:
-      if line[ex_zh_field] in seen_examples: continue
-      seen_examples.add(line[ex_zh_field])
-      out_lines[-1].append(line[ex_zh_field])
-      out_lines[-1].append(line[ex_en_field])
 
+  if ci_j not in EXAMPLES:
+    if "missing" not in EXAMPLES:
+      EXAMPLES["missing"] = []
+      EXAMPLES["missing"].append(ci_j)
+  else:
+    seen_examples = set()
+    for ex_zh, ex_en in zip(EXAMPLES[ci_j]["zh"], EXAMPLES[ci_j]["en"]):
+      if ex_zh in seen_examples: continue
+      seen_examples.add(ex_zh)
+      out_line[0].append(ex_zh)
+      out_line[0].append(ex_en)
+  out_lines.append(out_line[0])
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+for xx in out_lines:
+    print(xx)
   
 out_lines = [
         [field.replace("\n", NEWLINE) for field in out_line]
@@ -150,3 +175,25 @@ out_lines = [
 with open(fname_out, 'w') as csvfile:
   csvwriter = csv.writer(csvfile, delimiter =';', quoting=csv.QUOTE_ALL)
   csvwriter.writerows(out_lines)
+
+
+
+TARGET_CI = set(TARGET_CI)
+
+missing = TARGET_CI - DEFINITIONS.keys() 
+print(f"missing from DEFINITIONS: {'; '.join(missing)}\n")
+
+missing = TARGET_CI - PINYIN.keys() 
+print(f"missing from PINYIN: {'; '.join(missing)}\n")
+
+
+missing = TARGET_CI - RELATED_WORDS.keys() 
+print(f"missing from RELATED_WORDS: {'; '.join(missing)}\n")
+
+missing = TARGET_CI - EXAMPLES.keys() 
+print(f"missing from EXAMPLES: {'; '.join(missing)}\n")
+
+
+
+print(f"missing from ZI_DEFS: {ZI_DEFS.get('missing', [])}\n")
+print(f"missing from USE_SAME_ZI: {USE_SAME_ZI.get('missing', [])}\n")
