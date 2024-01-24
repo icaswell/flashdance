071dc804678fe953b0a5e06b38a6aa0fbfccee99
Isaac Caswell <isaaccaswell91.com>
2024-01-23 23:35:37 -0800

    缪

--- flashcards/make_flashcards.py
+++ flashcards/make_flashcards.py
@@ -5,12 +5,7 @@ import random
 from collections import defaultdict
 
 
-# fname="/Users/icaswell/Documents/dancing_miao/flashcards/sample1.csv"
-fname_out="flashcards/sample1.flashcards.csv"
 
-
-# TSV: first col is the target ci
-input_ci_fname = "resources/definitions_and_pinyin/hsk1to6_zi_pinyin_def_sample.tsv"
 
 # TSV: maps ci to (short) definition
 definitions_fname = "resources/definitions_and_pinyin/hsk1to6_zi_pinyin_def.tsv"
@@ -29,23 +24,63 @@ examples_fnames = [
         ["🐇", 2, "resources/example_sentences/cql.tsv"],
         ["☯", 2, "resources/example_sentences/daodejing.tsv"]
         # ☯️ ☯ ☾𖤓࿊
-        # ☯
-        # 
-    #   𓆝 𓆟 𓆞 𓆝 𓆟
-     
+        #   𓆝 𓆟 𓆞 𓆝 𓆟
 ]
 OBFUSTICATOR = "⠀" # invisible character to prevent deduping
 NEWLINE = "\r"
 MAX_EXAMPLES = 20
+MAX_EXAMPLES = 12
 
+# resources/vocab_separate/${X}.tsv
+# ci pinyin meaning
+# higher level is more advanced
+# fname="/Users/icaswell/Documents/dancing_miao/flashcards/sample1.csv"
+fname_out="flashcards/weeb1.flashcards.csv"
+TARGET_CI_LEVEL = "stront1"
+
+LEVELS = {
+    "hsk1.cedict": 1,
+    "hsk2.cedict": 2,
+    "hsk3.cedict": 3,
+    "hsk4.cedict": 4,
+    # "hsk1to6_zi_pinyin_def_sample": 4,
+    "hsk5.cedict": 5,
+    "hsk6.cedict": 6,
+    "stront1": 6,
+    "weeb1": 6,
+}
+
+LEVELED_CI = defaultdict(list)
+
+ALL_ZI_IN_LEVELS = set()
+for level in LEVELS:
+  with open(f"resources/vocab_separate/{level}.tsv", "r") as f:
+    for line in f:
+      ci, _, _ = line.split("\t", maxsplit=2)
+      LEVELED_CI[level].append(ci)
+      ALL_ZI_IN_LEVELS |= {zi for zi in ci}
+
+# zi: {level_name: [ci]}
+ZI_TO_LEVELED_CI = defaultdict(lambda: defaultdict(list))
+for zi in ALL_ZI_IN_LEVELS:
+  for level_name in LEVELS:
+    applicable_levels = {lev for lev in LEVELS if LEVELS[lev] <= LEVELS[level_name]}
+    for lev in applicable_levels:
+      for ci in LEVELED_CI[lev]:
+        if zi in ci:
+          ZI_TO_LEVELED_CI[zi][level_name].append(ci)
+# for lev in ZI_TO_LEVELED_CI["人"]:
+#     print(lev)
+#     print(ZI_TO_LEVELED_CI["人"][lev])
 
 def somple(X):
-  x = list(X.items())
-  print("\n=================================")
-  print(x[0:5], x[-5:])
+  pass
+  # x = list(X.items())
+  # print("\n=================================")
+  # print(x[0:5], x[-5:])
 
 TARGET_CI = []
-with open(input_ci_fname, "r") as f:
+with open(f"resources/vocab_separate/{TARGET_CI_LEVEL}.tsv", "r") as f:
   for line in f:
     TARGET_CI.append(line.strip().split("\t")[0])
 
@@ -58,36 +93,37 @@ with open(definitions_fname, "r") as f:
       if deff.startswith("/"):
         deff = deff[1:-1].replace("/", "; ")
       DEFINITIONS[parts[0]] = deff
-print("DEFINITIONS" + "\n"); somple(DEFINITIONS)
+# print("DEFINITIONS" + "\n"); somple(DEFINITIONS)
 
 PINYIN = {}
 with open(pinyin_fname, "r") as f:
-  for line in f:
+  for i, line in enumerate(f):
     parts = line.strip().split("\t")
-    PINYIN[parts[0]] = parts[1]
-print("PINYIN" + "\n"); somple(PINYIN)
+    if len(parts) != 2: print(i, line)
+    else: PINYIN[parts[0]] = parts[1]
+# print("PINYIN" + "\n"); somple(PINYIN)
 
 ZI_DEFS = {}
 with open(zi_definitions_fname, "r") as f:
   for line in f:
     parts = line.strip().split("\t")
     ZI_DEFS[parts[0]] = parts[1]
-print("ZI_DEFS" + "\n"); somple(ZI_DEFS)
+# print("ZI_DEFS" + "\n"); somple(ZI_DEFS)
 
 with open(multizi_definitions_fname, "r") as f:
   for line in f:
     parts = line.strip().split("\t")
     ZI_DEFS[parts[0]] = parts[1]
-print("ZI_DEFS" + "\n"); somple(ZI_DEFS)
+# print("ZI_DEFS" + "\n"); somple(ZI_DEFS)
 
 
-# i zi  first_occurence hsk1  hsk2  hsk3  hsk4  hsk5  hsk6  nhsk1 nhsk2 nhsk3 nhsk4 nhsk5 nhsk6 stront1 weeb1  
-USE_SAME_ZI = {}
-with open(use_same_zi_fname, "r") as f:
-  for line in f:
-    parts = line.strip().split("\t")
-    USE_SAME_ZI[parts[1]] = parts[-1]  # TODO handle logic for only the level below and equal
-print("USE_SAME_ZI" + "\n"); somple(USE_SAME_ZI)
+# # i zi  first_occurence hsk1  hsk2  hsk3  hsk4  hsk5  hsk6  nhsk1 nhsk2 nhsk3 nhsk4 nhsk5 nhsk6 stront1 weeb1  
+# USE_SAME_ZI = {}
+# with open(use_same_zi_fname, "r") as f:
+#   for line in f:
+#     parts = line.strip().split("\t")
+#     USE_SAME_ZI[parts[1]] = parts[-1]  # TODO handle logic for only the level below and equal
+# print("USE_SAME_ZI" + "\n"); somple(USE_SAME_ZI)
 
 # RELATED_WORDS = {}
 # with open(relatedwords_fname, "r") as f:
@@ -101,7 +137,7 @@ with open(relatedwords_explanation_fname, "r") as f:
   reader = csv.reader(f, delimiter=';')
   for row in reader:
     RELATED_WORDS[row[0]] = row[1]
-print("RELATED_WORDS" + "\n"); somple(RELATED_WORDS)
+# print("RELATED_WORDS" + "\n"); somple(RELATED_WORDS)
 
 EXAMPLES = defaultdict(list)
 for example_emoji, n_examples, examples_fname in examples_fnames:
@@ -124,7 +160,7 @@ for example_emoji, n_examples, examples_fname in examples_fnames:
       for zhex, enex in paired_examples[0:n_examples]:
         if ci not in zhex: continue
         EXAMPLES[ci].append((example_emoji, zhex, enex))
-print("EXAMPLES" + "\n"); somple(EXAMPLES)
+# print("EXAMPLES" + "\n"); somple(EXAMPLES)
 
 
 INVERTED_EXAMPLES = defaultdict(list)
@@ -135,7 +171,7 @@ for ci in TARGET_CI:
       if ci in zhex:
         # INVERTED_EXAMPLES[ci].append(("⏿" + example_emoji, zhex, enex))
         INVERTED_EXAMPLES[ci].append(("♻" + example_emoji, zhex, enex))
-print("EXAMPLES" + "\n"); somple(EXAMPLES)
+# print("EXAMPLES" + "\n"); somple(EXAMPLES)
 
 for ci, example_tups in INVERTED_EXAMPLES.items():
   EXAMPLES[ci] += example_tups
@@ -150,6 +186,18 @@ def pinyin(s):
   p = p.replace(" ？", "?")
   return p
 
+def format_cls(s):
+  # if "CL:" not in s: return s
+  s = re.sub("\p{Han}\|(\p{Han})\[", "\\1[", s)
+  # pinyins = re.findall("[\p{Han}]\[([a-z1-5]{2,7})\].?", s)
+  pinyins = re.findall("[^a-z]([a-z]{1,7}[1-5])[^a-z0-9]", s)
+  for p in pinyins:
+    p_better = pinyin_jyutping_sentence.romanization_conversion.decode_pinyin(p, False, False) 
+    s = s.replace(p, p_better)
+  return s
+
+  
+
 def conditional_append(ci, resource, out_line, field_name=None):
   if ci in resource:
     if field_name: out_line[0].append(field_name)
@@ -162,9 +210,10 @@ def conditional_append(ci, resource, out_line, field_name=None):
 
 MISSING_OTHER_CI_PINYIN = []
 MISSING_OTHER_CI_MEANING = []
-def get_other_ci_list(zi_j):
+def get_other_ci_list(zi_j, level):
   other_ci_list = []
-  other_ci_superset = USE_SAME_ZI[zi_j].split(";") if zi_j in USE_SAME_ZI else []
+  other_ci_superset = ZI_TO_LEVELED_CI[zi_j][level]
+  # other_ci_superset = USE_SAME_ZI[zi_j].split(";") if zi_j in USE_SAME_ZI else []
   for ci_k in other_ci_superset:
     ci_k = ci_k.strip()
     if ci_k == ci_j: continue
@@ -188,11 +237,13 @@ out_lines = []
 for ci_j in TARGET_CI:
   out_line = [[ci_j + OBFUSTICATOR]]
   if not conditional_append(ci_j, PINYIN, out_line): continue
-  if not conditional_append(ci_j, DEFINITIONS, out_line): continue
+  if ci_j in DEFINITIONS:
+    out_line[0].append(format_cls(DEFINITIONS[ci_j]))
+
   if not conditional_append(ci_j, RELATED_WORDS, out_line, "related words"): pass
 
   for zi_j in ci_j:
-    other_ci_list = get_other_ci_list(zi_j)
+    other_ci_list = get_other_ci_list(zi_j, TARGET_CI_LEVEL)
     zi_j_decorated = f"{zi_j} ({ZI_DEFS[zi_j]})" if zi_j in ZI_DEFS else zi_j
     content = "There are no other HSK words in this level (or before) using this character."
     if other_ci_list:
@@ -206,15 +257,15 @@ for ci_j in TARGET_CI:
     for ex_emoji, ex_zh, ex_en in EXAMPLES[ci_j][0:MAX_EXAMPLES]:
       if ex_zh in seen_examples: continue
       seen_examples.add(ex_zh)
-      ex_en = f"[{pinyin(ex_zh)}]{NEWLINE}{ex_en}"
+      ex_en = f"【{pinyin(ex_zh)}】{NEWLINE}{ex_en}"
       ex_zh = f"{ex_emoji} {ex_zh}"
       out_line[0].append(ex_zh)
       out_line[0].append(ex_en)
   out_lines.append(out_line[0])
 
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-for xx in out_lines:
-    print(xx)
+# print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+# for xx in out_lines:
+#     print(xx)
   
 out_lines = [
         [field.replace("\n", NEWLINE) for field in out_line]
@@ -248,9 +299,8 @@ print(f"missing from EXAMPLES: {'; '.join(missing)}\n")
 missing = TARGET_ZI - ZI_DEFS.keys() 
 print(f"missing from ZI_DEFS: {'; '.join(missing)}\n")
 
-missing = TARGET_ZI - USE_SAME_ZI.keys() 
-print(f"missing from USE_SAME_ZI: {'; '.join(missing)}\n")
-
 
 print(f"missing from MISSING_OTHER_CI_MEANING: {'; '.join(MISSING_OTHER_CI_MEANING)}\n")
 print(f"missing from MISSING_OTHER_CI_PINYIN: {'; '.join(MISSING_OTHER_CI_PINYIN)}\n")
+
+print(f"Wrote {fname_out}")
